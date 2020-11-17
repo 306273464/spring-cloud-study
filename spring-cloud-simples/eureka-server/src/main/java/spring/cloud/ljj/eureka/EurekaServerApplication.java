486c84a6d0bc915faf96ef445396a8656a53bdc9
lijj <lijj@citgc.com>
2020-11-17 10:48:05 +0800

    添加spring cloud 学习示例

---
+++ spring-cloud-simples/eureka-server/src/main/java/spring/cloud/ljj/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package spring.cloud.ljj.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ljj
+ * @Date 2020/11/16 0016
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
